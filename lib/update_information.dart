6cd9192ede3ecd2df60786e4c50b1cdc1fd6bb7e
chicuong <chicuong0215@gmail.com>
2022-11-01 20:01:46 +0700

    merge code tiendung; route (bag, shop, profile screen) to (menu-drawer screen);route (profile screen) to (update-information screen);fix other bug ui

--- lib/update_information.dart
+++ lib/update_information.dart
@@ -1,3 +1,4 @@
+import 'package:brain_hack/input_new_password.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
@@ -73,24 +74,26 @@ class _UpdateInformationState extends State<UpdateInformation> {
     Widget EditAvatar = Stack(
       alignment: Alignment.bottomCenter,
       children: [
-        Container(
+        const CircleAvatar(
+          backgroundColor: Color.fromARGB(255, 93, 182, 255),
+          radius: 100,
           child: CircleAvatar(
-            backgroundColor: Colors.green[800],
-            radius: 100,
-            child: const CircleAvatar(
-              radius: 90,
-              child: Image(
-                image: AssetImage('images/icon/logo.png'),
-              ),
+            backgroundColor: Color.fromARGB(255, 222, 241, 255),
+            radius: 95,
+            child: Image(
+              image: AssetImage('images/icon/logo_v2.png'),
             ),
           ),
         ),
-        Container(
+        SizedBox(
           height: 52,
           child: IconButton(
             iconSize: 50,
             onPressed: () {},
-            icon: Icon(Icons.photo_camera_outlined),
+            icon: const Icon(
+              Icons.photo_camera,
+              color: Color.fromARGB(255, 93, 182, 255),
+            ),
           ),
         ),
       ],
@@ -112,207 +115,221 @@ class _UpdateInformationState extends State<UpdateInformation> {
       }),
     );
     return Scaffold(
-      body: Center(
-        child: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("images/bg.jpg"),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Column(
-            children: [
-              Container(
-                child: Title,
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 25),
-                child: EditAvatar,
-              ),
-              Container(
-                height: 380,
-                margin: EdgeInsets.only(top: 15, left: 50, right: 30),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      height: 45,
-                      child: Row(
-                        children: [
-                          Container(
-                            child: Text(
-                              'TÊN :',
-                              style: GoogleFonts.bungee(
-                                textStyle: const TextStyle(
-                                    fontSize: 25, color: Colors.orange),
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: Padding(
-                              child: txtEditName,
-                              padding: EdgeInsets.only(left: 60),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      height: 45,
-                      child: Row(
-                        children: [
-                          Container(
-                            child: Text(
-                              'EMAIL :',
-                              style: GoogleFonts.bungee(
-                                textStyle: const TextStyle(
-                                    fontSize: 25, color: Colors.orange),
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: Padding(
-                              child: txtEditMail,
-                              padding: EdgeInsets.only(left: 30),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      child: Row(
-                        children: [
-                          Container(
-                            child: Text(
-                              'GIỚI TÍNH :',
-                              style: GoogleFonts.bungee(
-                                textStyle: const TextStyle(
-                                    fontSize: 25, color: Colors.orange),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            child: rbtnMale,
-                          ),
-                          Container(
-                            child: Text(
-                              'NAM',
-                              style: GoogleFonts.bungee(
-                                textStyle: const TextStyle(
-                                    fontSize: 20, color: Colors.orange),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            child: rbtnFemale,
-                          ),
-                          Container(
-                            child: Text(
-                              'NỮ',
-                              style: GoogleFonts.bungee(
-                                textStyle: const TextStyle(
-                                    fontSize: 20, color: Colors.orange),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      child: Row(
-                        children: [
-                          Container(
-                            child: Text(
-                              'NGÀY SINH :',
-                              style: GoogleFonts.bungee(
-                                textStyle: const TextStyle(
-                                    fontSize: 25, color: Colors.orange),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(left: 50),
-                            child: IconButton(
-                              color: Colors.green,
-                              iconSize: 50,
-                              onPressed: () {},
-                              icon: Icon(Icons.calendar_month_sharp),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                primary: Colors.red,
-                                padding: EdgeInsets.all(20),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10)),
-                              ),
-                              onPressed: () {},
-                              child: const Text(
-                                'TRỞ VỀ',
-                                style: TextStyle(
-                                    fontSize: 18, color: Colors.white),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                primary: Colors.blue,
-                                padding: EdgeInsets.all(10),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10)),
-                              ),
-                              onPressed: () {},
-                              child: Column(
-                                children: const [
-                                  Text(
-                                    'ĐỔI MẬT',
-                                    style: TextStyle(
-                                        fontSize: 18, color: Colors.white),
-                                  ),
-                                  Text(
-                                    'KHẨU',
-                                    style: TextStyle(
-                                        fontSize: 18, color: Colors.white),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          Container(
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                primary: Colors.green,
-                                padding: EdgeInsets.all(20),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10)),
-                              ),
-                              onPressed: () {},
-                              child: const Text(
-                                'LƯU',
-                                style: TextStyle(
-                                    fontSize: 18, color: Colors.white),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+        body: SingleChildScrollView(
+            child: Stack(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Image.asset(
+            "images/bg.jpg",
+            fit: BoxFit.cover,
           ),
         ),
-      ),
-    );
+        Column(
+          children: [
+            Container(
+              child: Title,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 25),
+              child: EditAvatar,
+            ),
+            Container(
+              height: 380,
+              margin: EdgeInsets.only(top: 15, left: 50, right: 30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 45,
+                    child: Row(
+                      children: [
+                        Container(
+                          child: Text(
+                            'TÊN :',
+                            style: GoogleFonts.bungee(
+                              textStyle: const TextStyle(
+                                  fontSize: 21, color: Colors.orange),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            child: txtEditName,
+                            padding: EdgeInsets.only(left: 60),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 45,
+                    child: Row(
+                      children: [
+                        Container(
+                          child: Text(
+                            'EMAIL :',
+                            style: GoogleFonts.bungee(
+                              textStyle: const TextStyle(
+                                  fontSize: 21, color: Colors.orange),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            child: txtEditMail,
+                            padding: EdgeInsets.only(left: 30),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Row(
+                      children: [
+                        Container(
+                          child: Text(
+                            'GIỚI TÍNH :',
+                            style: GoogleFonts.bungee(
+                              textStyle: const TextStyle(
+                                  fontSize: 21, color: Colors.orange),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: rbtnMale,
+                        ),
+                        Container(
+                          child: Text(
+                            'NAM',
+                            style: GoogleFonts.bungee(
+                              textStyle: const TextStyle(
+                                  fontSize: 20, color: Colors.orange),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: rbtnFemale,
+                        ),
+                        Container(
+                          child: Text(
+                            'NỮ',
+                            style: GoogleFonts.bungee(
+                              textStyle: const TextStyle(
+                                  fontSize: 20, color: Colors.orange),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Row(
+                      children: [
+                        Container(
+                          child: Text(
+                            'NGÀY SINH :',
+                            style: GoogleFonts.bungee(
+                              textStyle: const TextStyle(
+                                  fontSize: 21, color: Colors.orange),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 50),
+                          child: IconButton(
+                            color: Colors.green,
+                            iconSize: 50,
+                            onPressed: () {},
+                            icon: Icon(Icons.calendar_month_sharp),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.red,
+                              padding: EdgeInsets.all(20),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                            ),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text(
+                              'TRỞ VỀ',
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.blue,
+                              padding: EdgeInsets.all(10),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  PageRouteBuilder(
+                                    pageBuilder: (_, __, ___) =>
+                                        const InputNewPassword(),
+                                    transitionDuration:
+                                        const Duration(milliseconds: 500),
+                                    transitionsBuilder: (_, a, __, c) =>
+                                        FadeTransition(opacity: a, child: c),
+                                  ));
+                            },
+                            child: Column(
+                              children: const [
+                                Text(
+                                  'ĐỔI MẬT',
+                                  style: TextStyle(
+                                      fontSize: 18, color: Colors.white),
+                                ),
+                                Text(
+                                  'KHẨU',
+                                  style: TextStyle(
+                                      fontSize: 18, color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.green,
+                              padding: EdgeInsets.all(20),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                            ),
+                            onPressed: () {},
+                            child: const Text(
+                              'LƯU',
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        )
+      ],
+    )));
   }
 }
