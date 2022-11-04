11437cdbd0d34af86b8e17113cd8d02f22eacf64
chicuong <chicuong0215@gmail.com>
2022-11-04 11:46:28 +0700

    add ....

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,9 @@
 import 'dart:ffi';
 
-import 'package:brain_hack/about.dart';
-import 'package:brain_hack/bag.dart';
-import 'package:brain_hack/create_room.dart';
-import 'package:brain_hack/edit_room.dart';
-import 'package:brain_hack/field_rank.dart';
 import 'package:brain_hack/intro.dart';
-import 'package:brain_hack/list_friend.dart';
-import 'package:brain_hack/login.dart';
 import 'package:brain_hack/menu.dart';
-import 'package:brain_hack/playing.dart';
-import 'package:brain_hack/profile.dart';
-import 'package:brain_hack/ranking.dart';
-import 'package:brain_hack/room_item.dart';
-import 'package:brain_hack/rooms.dart';
-import 'package:brain_hack/waiting_room.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-import 'create_train.dart';
-import 'friend_item.dart';
 
 void main() {
   runApp(MyApp());
