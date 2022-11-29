9f7c5b7e32216f39d561a773332f1e20486c1cde
chicuong <chicuong0215@gmail.com>
2022-11-29 16:44:18 +0700

    fix register

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-import 'package:brain_hack/field_rank.dart';
-import 'package:brain_hack/friend_item.dart';
-import 'package:brain_hack/friend_profile.dart';
 import 'package:brain_hack/list_friend.dart';
 import 'package:brain_hack/login.dart';
-import 'package:brain_hack/menu.dart';
-import 'package:brain_hack/register.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -23,7 +18,7 @@ class MyApp extends StatelessWidget {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ListFriend(),
+      home: Login(),
     );
   }
 }
