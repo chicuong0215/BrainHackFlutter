162bb6acd68f2869fc0973148d3e1f7c295a2752
chicuong <chicuong0215@gmail.com>
2022-12-06 16:11:35 +0700

    update playing

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:brain_hack/list_friend.dart';
 import 'package:brain_hack/login.dart';
+import 'package:brain_hack/playing.dart';
+import 'package:brain_hack/profile.dart';
+import 'package:brain_hack/rooms.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -18,7 +21,7 @@ class MyApp extends StatelessWidget {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Login(),
+      home: Playing(),
     );
   }
 }
